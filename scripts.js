1da2b242f4d493f6d408352ffa529f3eea09a7f8
William <william.fujimori@gmail.com>
2022-04-06 15:29:22 -0300

    layout estatico e responsivo

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let numCartas;
-quantasCartas();
+
+//quantasCartas();
+
 function quantasCartas(){
     numCartas = prompt("Insira a quantidade de cartas entre 4 e 14 (números pares)");
     let numPar = Number(numCartas)%2;
+
     if(Number(numCartas) >= 4 && Number(numCartas) <= 14 && numPar === 0){
-        console.log("ta certo")
+        console.log("ta certo");
+
     } else {
-        alert("Por favor insira um número válido!")
-        quantasCartas()
+        alert("Por favor insira um número válido!");
+        quantasCartas();
     }
 }
