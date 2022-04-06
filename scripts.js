4890ac35b00cf3ea5c7324f1ace437092ddbfee9
William <william.fujimori@gmail.com>
2022-04-06 16:48:36 -0300

    Adicionar cartas

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let numCartas;
 
-//quantasCartas();
+quantasCartas();
 
 function quantasCartas(){
     numCartas = prompt("Insira a quantidade de cartas entre 4 e 14 (números pares)");
@@ -8,9 +8,22 @@ function quantasCartas(){
 
     if(Number(numCartas) >= 4 && Number(numCartas) <= 14 && numPar === 0){
         console.log("ta certo");
-
+        adicionarCartas();
     } else {
         alert("Por favor insira um número válido!");
         quantasCartas();
     }
+}
+
+
+function adicionarCartas(){
+    let cont = 0;
+    let divCartas = document.querySelector(".cartas");
+    console.log(divCartas)
+    while (cont < numCartas){
+        divCartas.innerHTML = divCartas.innerHTML + `<div class="cartaVirada"><img src="/Projeto_4_parrotsCardGame/imagens/front.png"></div>`
+        console.log(divCartas)
+        cont++;
+    }
+
 }
