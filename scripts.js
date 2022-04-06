b110f265dd42348298e0cb79ac3c9ed654a3982b
William <william.fujimori@gmail.com>
2022-04-06 10:34:57 -0300

    Número de cartas certo

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let numCartas;
+quantasCartas();
+function quantasCartas(){
+    numCartas = prompt("Insira a quantidade de cartas entre 4 e 14 (números pares)");
+    let numPar = Number(numCartas)%2;
+    if(Number(numCartas) >= 4 && Number(numCartas) <= 14 && numPar === 0){
+        console.log("ta certo")
+    } else {
+        alert("Por favor insira um número válido!")
+        quantasCartas()
+    }
+}
