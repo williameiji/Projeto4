b3e4a27fcb2dec9e4ab4a2b5c39375262d266156
William <william.fujimori@gmail.com>
2022-04-07 09:47:11 -0300

    selecionar itens da array

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 let numCartas;
 
+let gifs = ["/Projeto_4_parrotsCardGame/imagens/bobrossparrot.gif", "/Projeto_4_parrotsCardGame/imagens/explodyparrot.gif", "fiestaparrot.gif", "/Projeto_4_parrotsCardGame/imagens/metalparrot.gif", "/Projeto_4_parrotsCardGame/imagens/revertitparrot.gif", "/Projeto_4_parrotsCardGame/imagens/tripletsparrot.gif", "/Projeto_4_parrotsCardGame/imagens/unicornparrot.gif"]
+
+
+let selecGifs = [];
+let dobroGifs = [];
+
+
 quantasCartas();
 
 function quantasCartas(){
@@ -7,23 +14,46 @@ function quantasCartas(){
     let numPar = Number(numCartas)%2;
 
     if(Number(numCartas) >= 4 && Number(numCartas) <= 14 && numPar === 0){
-        console.log("ta certo");
+        selecionarCartas();
         adicionarCartas();
+        
     } else {
         alert("Por favor insira um número válido!");
         quantasCartas();
     }
 }
 
+function selecionarCartas () {
+    let num = numCartas / 2;
+
+    for (let i = 0; i < num; i++){
+        selecGifs.push(gifs[i]);
+        dobroGifs = selecGifs.concat(selecGifs);
+        console.log(dobroGifs)
+    }
+}
 
 function adicionarCartas(){
     let cont = 0;
-    let divCartas = document.querySelector(".cartas");
-    console.log(divCartas)
+    let divCartas = document.querySelector(".teste");
     while (cont < numCartas){
-        divCartas.innerHTML = divCartas.innerHTML + `<div class="cartaVirada"><img src="/Projeto_4_parrotsCardGame/imagens/front.png"></div>`
-        console.log(divCartas)
+        divCartas.innerHTML = divCartas.innerHTML + `<div class="cartas">
+        <div class="cartaFlip" onclick="trocarCarta(this)">
+            <div class="cartaFrente">
+                <img src="${dobroGifs[cont]}" >
+            </div>
+            <div class="cartaCostas" >
+                <img src="/Projeto_4_parrotsCardGame/imagens/front.png">
+            </div>
+        </div>
+    </div>`
+    console.log(divCartas.innerHTML)
         cont++;
     }
+}
+
+function trocarCarta(teste){
+    teste = teste.classList.add("flip");
+    
 
 }
