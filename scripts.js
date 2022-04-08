34c872c6994e16c494ad4dee99b69f3ed08197fb
William <william.fujimori@gmail.com>
2022-04-08 09:59:12 -0300

    condição das cartas feito

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let numCartas;
 
-let gifs = ["/Projeto_4_parrotsCardGame/imagens/bobrossparrot.gif", "/Projeto_4_parrotsCardGame/imagens/explodyparrot.gif", "fiestaparrot.gif", "/Projeto_4_parrotsCardGame/imagens/metalparrot.gif", "/Projeto_4_parrotsCardGame/imagens/revertitparrot.gif", "/Projeto_4_parrotsCardGame/imagens/tripletsparrot.gif", "/Projeto_4_parrotsCardGame/imagens/unicornparrot.gif"]
+let gifs = ["/Projeto_4_parrotsCardGame/imagens/bobrossparrot.gif", "/Projeto_4_parrotsCardGame/imagens/explodyparrot.gif", "/Projeto_4_parrotsCardGame/imagens/fiestaparrot.gif", "/Projeto_4_parrotsCardGame/imagens/metalparrot.gif", "/Projeto_4_parrotsCardGame/imagens/revertitparrot.gif", "/Projeto_4_parrotsCardGame/imagens/tripletsparrot.gif", "/Projeto_4_parrotsCardGame/imagens/unicornparrot.gif"]
 
 
 let selecGifs = [];
@@ -29,31 +29,77 @@ function selecionarCartas () {
     for (let i = 0; i < num; i++){
         selecGifs.push(gifs[i]);
         dobroGifs = selecGifs.concat(selecGifs);
-        console.log(dobroGifs)
     }
+}
+
+
+function embaralhar(){
+    return Math.random() - 0.5;
 }
 
 function adicionarCartas(){
     let cont = 0;
     let divCartas = document.querySelector(".teste");
+    dobroGifs = dobroGifs.sort(embaralhar);
     while (cont < numCartas){
-        divCartas.innerHTML = divCartas.innerHTML + `<div class="cartas">
-        <div class="cartaFlip" onclick="trocarCarta(this)">
-            <div class="cartaFrente">
-                <img src="${dobroGifs[cont]}" >
+        divCartas.innerHTML = divCartas.innerHTML + 
+        `<div class="cartas">
+            <div class="cartaFlip" onclick="trocarCarta(this)">
+                <div class="cartaFrente">
+                    <img src="${dobroGifs[cont]}" >
+                </div>
+                <div class="cartaCostas" >
+                    <img src="/Projeto_4_parrotsCardGame/imagens/front.png">
+                </div>
             </div>
-            <div class="cartaCostas" >
-                <img src="/Projeto_4_parrotsCardGame/imagens/front.png">
-            </div>
-        </div>
-    </div>`
-    console.log(divCartas.innerHTML)
+        </div>`
+    
         cont++;
     }
 }
 
 function trocarCarta(teste){
-    teste = teste.classList.add("flip");
+    teste.classList.add("flip");
+    setTimeout(compararCartas, 2000);
     
+}
 
+
+function compararCartas(){
+    let x = document.querySelectorAll(".flip .cartaFrente img");
+    let z = document.querySelectorAll(".cartaFlip.flip");
+    let y = document.querySelector(".cartaFlip .flip");
+
+    
+    if (x.length > 1){
+        let a = x[0].src;
+        let b = x[1].src;
+       
+
+        let i = 0;
+        if (a != b){
+
+            
+            while(i < z.length) {
+                
+                z[i].classList.remove("flip")
+                //x = [""];
+                //z = [""];
+                i++;
+            }
+            
+        }else {
+            while(i < z.length) {
+            z[i].classList.add("flipOk")
+            z[i].classList.remove("flip")
+            i++;
+        }
+        }
+
+    }   
+    x = [""];
+                z = [""];
+
+    console.log(x)
+    console.log(z)
 }
